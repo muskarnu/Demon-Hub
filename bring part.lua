a31b43fc366dc69bf127a07df2371869de2d96e8
AHMED <ramik7233@gmail.com>
2025-06-12 05:05:57 +0300

    Update bring part.lua

--- bring part.lua
+++ bring part.lua
@@ -1,6 +1,10 @@
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
 local Gui = Instance.new("ScreenGui")
 local Main = Instance.new("Frame")
-local ToggleButton = Instance.new("ImageButton")
 local Box = Instance.new("TextBox")
 local UITextSizeConstraint = Instance.new("UITextSizeConstraint")
 local Label = Instance.new("TextLabel")
@@ -8,43 +12,35 @@ local UITextSizeConstraint_2 = Instance.new("UITextSizeConstraint")
 local Button = Instance.new("TextButton")
 local UITextSizeConstraint_3 = Instance.new("UITextSizeConstraint")
 
+--Properties:
+
 Gui.Name = "Gui"
-Gui.Parent = gethui and gethui() or game.CoreGui
+Gui.Parent = gethui()
 Gui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 Main.Name = "Main"
 Main.Parent = Gui
-Main.BackgroundColor3 = Color3.fromRGB(139, 0, 0)
-Main.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Main.BorderSizePixel = 2
-Main.Position = UDim2.new(0.35, 0, 0.4, 0)
-Main.Size = UDim2.new(0.24, 0, 0.18, 0)
+Main.BackgroundColor3 = Color3.fromRGB(120, 0, 0) -- احمر غامق
+Main.BorderColor3 = Color3.fromRGB(0, 0, 0) -- اسود
+Main.BorderSizePixel = 4 -- خليت الإطار واضح
+Main.Position = UDim2.new(0.335954279, 0, 0.542361975, 0)
+Main.Size = UDim2.new(0.240350261, 0, 0.166880623, 0)
 Main.Active = true
 Main.Draggable = true
 
-ToggleButton.Name = "ToggleButton"
-ToggleButton.Parent = Gui
-ToggleButton.BackgroundTransparency = 0.3
-ToggleButton.BackgroundColor3 = Color3.fromRGB(139, 0, 0)
-ToggleButton.BorderSizePixel = 0
-ToggleButton.Position = UDim2.new(0.01, 0, 0.45, 0)
-ToggleButton.Size = UDim2.new(0.05, 0, 0.09, 0)
-ToggleButton.Image = "rbxassetid://107728948792167"
-ToggleButton.Active = true
-ToggleButton.Draggable = true
-
 Box.Name = "Box"
 Box.Parent = Main
-Box.BackgroundColor3 = Color3.fromRGB(139, 0, 0)
+Box.BackgroundColor3 = Color3.fromRGB(95, 95, 95)
 Box.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Box.BorderSizePixel = 2
-Box.Position = UDim2.new(0.1, 0, 0.25, 0)
-Box.Size = UDim2.new(0.8, 0, 0.35, 0)
+Box.BorderSizePixel = 0
+Box.Position = UDim2.new(0.0980926454, 0, 0.218712583, 0)
+Box.Size = UDim2.new(0.801089942, 0, 0.364963502, 0)
 Box.FontFace = Font.new("rbxasset://fonts/families/SourceSansSemibold.json", Enum.FontWeight.Bold, Enum.FontStyle.Normal)
-Box.PlaceholderText = "اكتب الاستهداف"
+Box.PlaceholderText = "Player here"
 Box.Text = ""
 Box.TextColor3 = Color3.fromRGB(255, 255, 255)
 Box.TextScaled = true
+Box.TextSize = 31.000
 Box.TextWrapped = true
 
 UITextSizeConstraint.Parent = Box
@@ -52,14 +48,15 @@ UITextSizeConstraint.MaxTextSize = 31
 
 Label.Name = "Label"
 Label.Parent = Main
-Label.BackgroundColor3 = Color3.fromRGB(139, 0, 0)
+Label.BackgroundColor3 = Color3.fromRGB(95, 95, 95)
 Label.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Label.BorderSizePixel = 2
-Label.Size = UDim2.new(1, 0, 0.16, 0)
+Label.BorderSizePixel = 0
+Label.Size = UDim2.new(1, 0, 0.160583943, 0)
 Label.FontFace = Font.new("rbxasset://fonts/families/Nunito.json", Enum.FontWeight.Bold, Enum.FontStyle.Normal)
-Label.Text = "المطور: Rami | BY @Sajwaad"
+Label.Text = "By:Sajwaad|المطورRami"
 Label.TextColor3 = Color3.fromRGB(255, 255, 255)
 Label.TextScaled = true
+Label.TextSize = 14.000
 Label.TextWrapped = true
 
 UITextSizeConstraint_2.Parent = Label
@@ -67,127 +64,25 @@ UITextSizeConstraint_2.MaxTextSize = 21
 
 Button.Name = "Button"
 Button.Parent = Main
-Button.BackgroundColor3 = Color3.fromRGB(139, 0, 0)
+Button.BackgroundColor3 = Color3.fromRGB(95, 95, 95)
 Button.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Button.BorderSizePixel = 2
-Button.Position = UDim2.new(0.18, 0, 0.65, 0)
-Button.Size = UDim2.new(0.63, 0, 0.28, 0)
+Button.BorderSizePixel = 0
+Button.Position = UDim2.new(0.183284417, 0, 0.656760991, 0)
+Button.Size = UDim2.new(0.629427791, 0, 0.277372271, 0)
 Button.Font = Enum.Font.Nunito
-Button.Text = "Bring | مطفي"
+Button.Text = "Bring | Off"
 Button.TextColor3 = Color3.fromRGB(255, 255, 255)
 Button.TextScaled = true
+Button.TextSize = 28.000
 Button.TextWrapped = true
 
 UITextSizeConstraint_3.Parent = Button
 UITextSizeConstraint_3.MaxTextSize = 28
-
-local Players = game:GetService("Players")
-local RunService = game:GetService("RunService")
-local LocalPlayer = Players.LocalPlayer
-local Workspace = game:GetService("Workspace")
-
-local character
-local humanoidRootPart
-
-local Folder = Instance.new("Folder", Workspace)
-local Part = Instance.new("Part", Folder)
-local Attachment1 = Instance.new("Attachment", Part)
-Part.Anchored = true
-Part.CanCollide = false
-Part.Transparency = 1
-
-local mainStatus = true
-
-ToggleButton.MouseButton1Click:Connect(function()
-	mainStatus = not mainStatus
-	Main.Visible = mainStatus
-end)
-
-if not getgenv().Network then
-	getgenv().Network = {
-		BaseParts = {},
-		Velocity = Vector3.new(24.46, 24.46, 24.46)
-	}
-
-	Network.RetainPart = function(Part)
-		if Part:IsA("BasePart") and Part:IsDescendantOf(Workspace) then
-			table.insert(Network.BaseParts, Part)
-			Part.CustomPhysicalProperties = PhysicalProperties.new(0, 0, 0, 0, 0)
-			Part.CanCollide = false
-		end
-	end
-
-	local function EnablePartControl()
-		LocalPlayer.ReplicationFocus = Workspace
-		RunService.Heartbeat:Connect(function()
-			sethiddenproperty(LocalPlayer, "SimulationRadius", math.huge)
-			for _, Part in pairs(Network.BaseParts) do
-				if Part:IsDescendantOf(Workspace) then
-					Part.Velocity = Network.Velocity
-				end
-			end
-		end)
-	end
-
-	EnablePartControl()
-end
-
-local function ForcePart(v)
-	if v:IsA("BasePart") and not v.Anchored and not v.Parent:FindFirstChildOfClass("Humanoid") and not v.Parent:FindFirstChild("Head") and v.Name ~= "Handle" then
-		for _, x in ipairs(v:GetChildren()) do
-			if x:IsA("BodyMover") or x:IsA("RocketPropulsion") then
-				x:Destroy()
-			end
-		end
-		if v:FindFirstChild("Attachment") then
-			v:FindFirstChild("Attachment"):Destroy()
-		end
-		if v:FindFirstChild("AlignPosition") then
-			v:FindFirstChild("AlignPosition"):Destroy()
-		end
-		if v:FindFirstChild("Torque") then
-			v:FindFirstChild("Torque"):Destroy()
-		end
-		v.CanCollide = false
-		local Torque = Instance.new("Torque", v)
-		Torque.Torque = Vector3.new(9900000, 9900000, 9900000)
-		local AlignPosition = Instance.new("AlignPosition", v)
-		local Attachment2 = Instance.new("Attachment", v)
-		Torque.Attachment0 = Attachment2
-		AlignPosition.MaxForce = math.huge
-		AlignPosition.MaxVelocity = math.huge
-		AlignPosition.Responsiveness = 400
-		AlignPosition.Attachment0 = Attachment2
-		AlignPosition.Attachment1 = Attachment1
-	end
-end
-
-local blackHoleActive = false
-local DescendantAddedConnection
-
-local function toggleBlackHole()
-	blackHoleActive = not blackHoleActive
-	if blackHoleActive then
-		Button.Text = "Bring | شغال"
-		for _, v in ipairs(Workspace:GetDescendants()) do
-			ForcePart(v)
-		end
-
-		DescendantAddedConnection = Workspace.DescendantAdded:Connect(function(v)
-			if blackHoleActive then
-				ForcePart(v)
-			end
-		end)
-
-		spawn(function()
-			while blackHoleActive and RunService.RenderStepped:Wait() do
-				if humanoidRootPart then
-					Attachment1.WorldCFrame = humanoidRootPart.CFrame
-				end
+				Attachment1.WorldCFrame = humanoidRootPart.CFrame
 			end
 		end)
 	else
-		Button.Text = "Bring | مطفي"
+		Button.Text = "Bring Parts | Off"
 		if DescendantAddedConnection then
 			DescendantAddedConnection:Disconnect()
 		end
@@ -197,7 +92,10 @@ end
 local function getPlayer(name)
 	local lowerName = string.lower(name)
 	for _, p in pairs(Players:GetPlayers()) do
-		if string.find(string.lower(p.Name), lowerName) or string.find(string.lower(p.DisplayName), lowerName) then
+		local lowerPlayer = string.lower(p.Name)
+		if string.find(lowerPlayer, lowerName) then
+			return p
+		elseif string.find(string.lower(p.DisplayName), lowerName) then
 			return p
 		end
 	end
@@ -205,19 +103,33 @@ end
 
 local player = nil
 
-Box.FocusLost:Connect(function(enterPressed)
-	if enterPressed then
-		player = getPlayer(Box.Text)
-		if player then
-			Box.Text = player.Name
-		end
-	end
-end)
+local function VDOYZQL_fake_script() -- Box.Script 
+	local script = Instance.new('Script', Box)
 
-Button.MouseButton1Click:Connect(function()
-	if player then
-		character = player.Character or player.CharacterAdded:Wait()
-		humanoidRootPart = character:WaitForChild("HumanoidRootPart")
-		toggleBlackHole()
-	end
-end)
+	script.Parent.FocusLost:Connect(function(enterPressed)
+		if enterPressed then
+			player = getPlayer(Box.Text)
+			if player then
+				Box.Text = player.Name
+				print("Player found:", player.Name)
+			else
+				print("Player not found")
+			end
+		end
+	end)
+end
+coroutine.wrap(VDOYZQL_fake_script)()
+local function JUBNQKI_fake_script() -- Button.Script 
+	local script = Instance.new('Script', Button)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		if player then
+			character = player.Character or player.CharacterAdded:Wait()
+			humanoidRootPart = character:WaitForChild("HumanoidRootPart")
+			toggleBlackHole()
+		else
+			print("Player is not selected")
+		end
+	end)
+end
+coroutine.wrap(JUBNQKI_fake_script)()
